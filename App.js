0cc6e1c65c2b47195048453cba26ab741ac0353a
bhavyee78 <68479422+bhavyee78@users.noreply.github.com>
2022-09-06 07:05:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react' ; 
+import Stock from './Stock' ; 
+import logo from './logo.svg';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Stock></Stock>
+    </div>
+  );
+}
+
+export default App;
---
+++ Stock.js
@@ -0,0 +1,67 @@
+import React from 'react' ; 
+import Plot from 'react-plotly.js';
+
+class Stock extends React.Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+          stockChartXValues: [],
+          stockChartYValues: []
+        }
+      }
+      componentDidMount () {
+        this.fetchStock();
+      }
+      fetchStock() {
+          const pointerToThis = this ; 
+          console.log(pointerToThis) ; 
+          const API_KEY = 'IW49RKR1MZ0QQULI' ; 
+          let Stocksymbol = 'AMZN' ; 
+          let API_Call = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&outputsize=full&apikey=IW49RKR1MZ0QQULI' ; 
+          let stockChartXValuesFunction =  [] ; 
+          let stockChartYValuesFunction = [] ; 
+
+          fetch(API_Call)
+            .then(
+                function(response){
+                    return response.json() ; 
+
+                }
+            )
+            .then(
+                function(data){
+                    console.log(data) ; 
+
+                    for(var key in data['Time Series (Daily)']){
+                    stockChartXValuesFunction.push(key) ; 
+                    stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open'])
+                }
+                // console.log(stockChartXValuesFunction) ; 
+                pointerToThis.setState({
+                    stockChartXValues: stockChartXValuesFunction,
+                    stockChartYValues: stockChartYValuesFunction
+                  });
+                }
+            )
+      }
+  render () {
+    return (
+      <div>
+        <h1>Stock Market</h1>
+        <Plot
+            data={[
+    {
+     x: this.state.stockChartXValues,
+      y: this.state.stockChartYValues,
+      type: 'scatter',
+      mode: 'lines+markers',
+      marker: {color: 'red'},
+    }
+ ]}
+  layout={{width: 820, height: 640, title: 'Chart'}}
+/>
+      </div>
+    )
+  }
+}
+export default Stock;
